ebfb44d9d7c92aa2b06357e7a2f9c389b555d3b4
detonih <54778020+detonih@users.noreply.github.com>
2020-02-17 21:59:25 -0300

    Revert "Henriquebranch"

--- backend/src/server.js
+++
@@ -1,5 +0,0 @@
-// Importa a classe App do arquivo "app.js"
-import app from './app';
-
-// A classe App importada dentro da variável "app" (que mantém o express), irá "ouvir" a porta 3333.
-app.listen(3333);
