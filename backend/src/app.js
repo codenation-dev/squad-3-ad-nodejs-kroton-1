f37f3701a1e264958a65eb3727b9150977f21f2d
Henrique Detoni <detonihenrique@gmail.com>
2020-02-19 11:06:47 -0300

    create routes for logs and users

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const routes = require('./routes');
+
 app.use(bodyParser.json());
+
+app.use('/', routes)
 
 module.exports = {
     app
--- backend/src/bin/server.js
+++ backend/src/bin/server.js
@@ -1,5 +1,5 @@
 const { app } = require('../app');
 
 app.listen(8080, () => {
-    console.log('Listening port 8080')
+    console.log('Listening at port 8080')
 })
