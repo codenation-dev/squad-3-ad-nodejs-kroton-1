3ba53b095d1d89d8332b3b05872093d91feb201b
Luane <luane.aquino.br@gmail.com>
2020-03-09 23:00:03 -0300

    change assignment to routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,8 @@
 const path = require('path')
-const routesPath = path.resolve('src/routes')
+const routes = require(path.resolve('src/routes'))
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
-const routes = require(routesPath);
 
 app.use(bodyParser.json());
 
