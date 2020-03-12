4a982d22e47ae4f919d074f39decdece4e428c8a
Henrique Detoni <detonihenrique@gmail.com>
2020-03-12 17:27:45 -0300

    modify log model

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'});
+require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? '../../.env.test' : '../../.env'});
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
   host: process.env.DB_HOST,
--- backend/src/models/Log.js
+++ backend/src/models/Log.js
@@ -2,10 +2,30 @@ const Sequelize = require('sequelize');
 const sequelizeConfig = require('../config/database');
 
 const Log = sequelizeConfig.define('Log', {
-  log: {
+  level: {
+    type: Sequelize.ENUM,
+    values: ['FATAL', 'ERROR', 'WARN', 'INFO', 'DEBUG', 'TRACE', 'ALL', 'OFF'],
+    allowNull: false
+  },
+  description: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+  sender_application: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  send_date: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.ENUM,
+    values: ['active', 'pending', 'deleted'],
+    defaultValue: 'active',
+    allowNull: false
   }
+
 });
 
 module.exports = { Log };
