1825f30304414107753566a5e508d72df6de45e0
dasilvaguilhermecesar <dasilvaguilhermecesar@gmail.com>
2020-03-11 21:46:15 -0300

    implemented module-alias in server.js

--- backend/src/bin/server.js
+++ backend/src/bin/server.js
@@ -1,6 +1,5 @@
-const path = require('path')
-const appPath = path.resolve('src/app')
-const { app } = require(appPath);
+require('module-alias/register')
+const { app } = require('@app');
 
 app.listen(8080, () => {
     console.log('Listening at port 8080')
