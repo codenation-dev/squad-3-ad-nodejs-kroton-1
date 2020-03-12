1be5a512b862f22de941164a34b9368ac11045c8
dasilvaguilhermecesar <dasilvaguilhermecesar@gmail.com>
2020-03-12 11:11:48 -0300

    alias removed from server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const routes = require('@routes')
+const routes = require('./routes')
 
 app.use(bodyParser.json());
 
