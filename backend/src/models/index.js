697ac91dbc437c25f23c58998bf59d5f3a2063ec
Juliano F. Ribeiro <julianofribeiro@gmail.com>
2020-03-17 18:29:53 -0300

    Nao permite chave estrangeira nula nos Logs

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -3,7 +3,13 @@ const { sequelize, Sequelize } = require('../config/database');
 const Log = require('./Log')(sequelize, Sequelize)
 const User = require('./User')(sequelize, Sequelize)
 
-User.hasMany(Log, { onDelete: 'restrict', onUpdate: 'restrict' })
+User.hasMany(Log, {
+    onDelete: 'restrict',
+    onUpdate: 'restrict',
+    foreignKey: {
+        allowNull: false
+    }
+})
 Log.belongsTo(User)
 
 module.exports = { User, Log, sequelize }
