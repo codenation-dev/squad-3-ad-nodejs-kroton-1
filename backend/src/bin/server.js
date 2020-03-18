0d61c7bc8480394e89582ed1010c6c348a8b0041
Henrique Detoni <detonihenrique@gmail.com>
2020-03-18 15:10:24 -0300

    refactoring server.js

--- backend/src/bin/server.js
+++ backend/src/bin/server.js
@@ -3,5 +3,5 @@ const { app } = require('../app');
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log('Listening at port 8080')
+    console.log(`Listening at port: ${port}`)
 })
