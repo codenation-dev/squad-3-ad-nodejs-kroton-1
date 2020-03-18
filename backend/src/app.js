660822d538d9c94d30a357a1bcc0fd72ed6e3b7d
Henrique Detoni <detonihenrique@gmail.com>
2020-03-18 14:59:12 -0300

    refactoring

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ const routes = require('./routes')
 
 app.use(bodyParser.json());
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use('/', routes)
 
 module.exports = {
--- backend/src/bin/databaseSynchronization.js
+++ backend/src/bin/databaseSynchronization.js
@@ -1,5 +1,5 @@
 const { sequelize } = require('../models')
-console.log(process.env.NODE_ENV, "ESTA AQUIIIIII")
+
 sequelize
   .sync({ alter: true })
   .then(() => console.log('Database synced on ' + process.env.NODE_ENV + ' mode'))
