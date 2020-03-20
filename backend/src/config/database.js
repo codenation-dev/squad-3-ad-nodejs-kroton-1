843ceda1887395e598e4366a19d7efb18581513b
Luane Aquino <luanecaroline91@gmail.com>
2020-03-20 16:55:41 -0300

    remove path from config function

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,5 @@
 const Sequelize = require('sequelize')
-const path = require('path')
-
-require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? path.resolve(__dirname, '../../.env.test') : path.resolve(__dirname, '../../.env.test') })
+require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
   host: process.env.DB_HOST,
